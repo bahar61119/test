938e6f9ebf314b391adf967e75e3c641becc8702
bahar61119 <bahar61119@gmail.com>
2015-06-16 02:28:58 +0600

    15/06/15

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -26,7 +26,7 @@ class Welcome extends CI_Controller {
             {
                 $answer = $r['answer'];
             }
-        } else if(strcmp ($str[0], 'good')) {
+        } else if(strcmp ($str[0], 'good')==0) {
            $answer = "Hello, Kitty! ";
            $answer .= $str[0]." ".$str[1]." I am Md Habibullah Bin Ismail. It's nice to meet you.";
         } else {
@@ -43,24 +43,79 @@ class Welcome extends CI_Controller {
     
     function weather()
     {
-       
-        $city="Delhi";
-        $country="IN"; //Two digit country code
-        $url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
-        $json=file_get_contents($url);
-        $data=json_decode($json,true);
-        //Get current Temperature in Celsius
-        echo $data['main']['temp']."<br>";
-        //Get weather condition
-        echo $data['weather'][0]['main']."<br>";
-        //Get cloud percentage
-        echo $data['clouds']['all']."<br>";
-        //Get wind speed
-        echo $data['wind']['speed']."<br>";
+  
         
-        $question = strtolower($this->input->get('q'));
+        $question = $this->input->get('q');
         //echo $question;  
-        $str = preg_split("/[\s,]+/",  $question);
+        $str = preg_split("/[\s,?]+/",  $question);
+        
+        $city_index = 0;
+        for($i=0;$i<count($str);$i++)
+        {
+            if(strcmp($str[$i], "in")==0)
+            {
+                $city_index = $i+1;
+                break;
+            }
+        }
+        $city = "";
+        
+        for($i=$city_index;$i<count($str);$i++)
+        {
+            $city.=$str[$i]." ";
+        }
+        
+        //$country="IN"; //Two digit country code
+        $url="http://api.openweathermap.org/data/2.5/weather?q=".$city;
+        
+        $json=file_get_contents($url);
+        //echo $json;
+        $data=json_decode($json,true);
+        //echo print_r($data);
+        
+        $answer = "";
+        foreach($str as $word)
+        {
+            if($word == "temperature")
+            {
+                $answer .= $data['main']['temp']. " K";
+                
+                break;
+            }
+            else if($word == "humidity") {
+                $answer .= $data['main']['humidity'];
+                break;
+            }
+            else  if($word == "Rain"){
+                if($data['weather'][0]['id']>=500 && $data['weather'][0]['id']<600)
+                {
+                    $answer .= "Yes";
+                }
+                else  $answer .= "No";
+                break;
+            }
+            else if($word == "Clouds"){
+                if($data['weather'][0]['id']>800 && $data['weather'][0]['id']<900)
+                {
+                    $answer .= "Yes";
+                }
+                else  $answer .= "No";
+                break;
+            }
+            else if($word == "Clear"){
+                if($data['weather'][0]['id']==800)
+                {
+                    $answer .= "Yes";
+                }
+                else  $answer .= "No";
+                break;
+            }
+        }
+        
+        $d['answer']= $answer;
+	$response = json_encode($d);
+        
+	echo $response;
   
     }
 
