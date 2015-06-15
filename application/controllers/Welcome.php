2d8962bdbbc7f082b44f3b7e36401096d87a58f7
bahar61119 <bahar61119@gmail.com>
2015-06-16 01:24:40 +0600

    first complete

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -13,19 +13,56 @@ class Welcome extends CI_Controller {
     
     function greetings()
     {
-        $question = $this->input->get('q');
-                
-        $data = preg_split("/[\s,]+/    ",  $question);
-	
+        $question = strtolower($this->input->get('q'));
+        //echo $question;  
+        $str = preg_split("/[\s,]+/",  $question);
+        //echo $str[0];  
+  
         $this->db->where('question', $question);
         $q = $this->db->get('greetings');
-        
-	$response = "Hello, Kitty! ";
-        
+        $answer = "";
+        if ($q->num_rows()) {
+            foreach($q->result_array() as $r)
+            {
+                $answer = $r['answer'];
+            }
+        } else if(strcmp ($str[0], 'good')) {
+           $answer = "Hello, Kitty! ";
+           $answer .= $str[0]." ".$str[1]." I am Md Habibullah Bin Ismail. It's nice to meet you.";
+        } else {
+           $answer = "Hello, Kitty! ";
+           $answer .= " I am Md Habibullah Bin Ismail. It's nice to meet you.";
+        }
+	$data['answer']= $answer;
+	$response = json_encode($data);
         
 	echo $response;
+    
         
-	}
+    }
+    
+    function weather()
+    {
+       
+        $city="Delhi";
+        $country="IN"; //Two digit country code
+        $url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
+        $json=file_get_contents($url);
+        $data=json_decode($json,true);
+        //Get current Temperature in Celsius
+        echo $data['main']['temp']."<br>";
+        //Get weather condition
+        echo $data['weather'][0]['main']."<br>";
+        //Get cloud percentage
+        echo $data['clouds']['all']."<br>";
+        //Get wind speed
+        echo $data['wind']['speed']."<br>";
+        
+        $question = strtolower($this->input->get('q'));
+        //echo $question;  
+        $str = preg_split("/[\s,]+/",  $question);
+  
+    }
 
     
 
